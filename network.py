9833bf5722db68c35d7a4e31eee6f1d824015780
samarthbhargav <samarth.bhargav92@gmail.com>
2018-11-17 19:10:58 +0100

    I sat on a rug biding my time

--- network.py
+++ network.py
@@ -164,6 +164,7 @@ class QuasiSiameseNetwork(object):
                 outputs = self.model(image1, image2)
                 _, preds = torch.max(outputs, 1)
                 _, labels = torch.max(labels, 1)
+                print(preds, labels)
                 loss = self.criterion(preds, labels)
 
                 if phase == 'train':
