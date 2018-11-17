430e2d62e4883f5dca7580e98b0be21bf58ab3b5
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2018-11-17 14:53:01 +0100

    dataset without processing

--- run.py
+++ run.py
@@ -1,76 +1,55 @@
 import os
 import sys
 
-import torch
+# from model import Model
+# from network import Siamese
+from data import loadDataset
+from utils import configuration
 
-import rasterio
-import geopandas
-import json
+import logging
 
-import rasterio.mask
-import rasterio.features
-import rasterio.warp
+# logging
 
-TEMP_FOLDER = os.path.join('.', 'temp')
+logger = logging.getLogger(__name__)
+logging.getLogger('Fiona').setLevel(logging.ERROR)
+logging.getLogger('fiona.collection').setLevel(logging.ERROR)
+logging.getLogger('rasterio').setLevel(logging.ERROR)
 
-DATA_FOLDER = os.path.join('.', 'data', 'RC Challenge 1', '1')
 
-BEFORE_FOLDER = os.path.join(DATA_FOLDER, 'Before')
+def exceptionLogger(exceptionType, exceptionValue, exceptionTraceback):
+    logger.error("Uncaught Exception", exc_info=(exceptionType, exceptionValue, exceptionTraceback))
 
-AFTER_FOLDER = os.path.join(DATA_FOLDER, 'After')
 
-GEOJSON_FOLDER = os.path.join(DATA_FOLDER, 'Building Info')
+sys.excepthook = exceptionLogger
 
-TRAINING_GEOJSON_FILE = os.path.join(GEOJSON_FOLDER, 'TrainingDataset.geojson')
-TEST_1_GEOJSON_FILE = os.path.join(GEOJSON_FOLDER, 'TestSet_1.geojson')
-TEST_2_GEOJSON_FILE = os.path.join(GEOJSON_FOLDER, 'TestSet_2.geojson')
-ALL_BUILDINGS_GEOJSON_FILE = os.path.join(GEOJSON_FOLDER, 'AllBuildingOutline.geojson')
 
-print('DATA FOLDER {}'.format(os.listdir(DATA_FOLDER)))
-print('BEFORE {}'.format(os.listdir(BEFORE_FOLDER)))
-print('AFTER {}'.format(os.listdir(AFTER_FOLDER)))
-print('GEOJSON {}'.format(os.listdir(GEOJSON_FOLDER)))
+if __name__ == '__main__':
+    args = configuration()
 
-all_buildings_df = geopandas.read_file(ALL_BUILDINGS_GEOJSON_FILE)
-training_df = geopandas.read_file(TRAINING_GEOJSON_FILE)
-test_1_df = geopandas.read_file(TEST_1_GEOJSON_FILE)
-test_2_df = geopandas.read_file(TEST_2_GEOJSON_FILE)
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler(os.path.join(args.checkpointPath, 'run.log')),
+            logging.StreamHandler(sys.stdout)
+        ],
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)s %(levelname)s %(message)s'
+    )
 
-print(len(all_buildings_df))
-print(len(training_df))
-print(len(test_1_df))
-print(len(test_2_df))
+    logger.info('START with Configuration : {}'.format(args))
 
-geometry = None
+    # Load Dataset
+    dataset = loadDataset(args, transforms=None)
 
-training_json = json.loads(training_df.to_json())
+    # Initialize Model
+    # model = Model(Siamese, data, args)
 
-for index, feature in enumerate(training_json['features']):
-	geometry = feature['geometry']
-	if (index+1)%2 == 0:
-		break
+    # if args.train:
+    #     trainResult = model.train(save=True)
+    #     # plt.plot(trainResult)
+    #     # plt.savefig(os.path.join(args.checkpointPath, 'trainingLoss.png'))
 
-print(geometry)
+    # model.eval()
+    # if args.eval is not None:
+    #     model.predict()
 
-BEFORE_FILE = os.path.join(BEFORE_FOLDER, 'IGN_Feb2017_20CM.tif')
-
-with rasterio.open(BEFORE_FILE) as src:
-    out_image, out_transform = rasterio.mask.mask(src, [geometry], crop=True)
-    print(out_image.shape)
-
-'''
-out_meta = src.meta.copy()
-
-# save the resulting raster  
-out_meta.update({
-	"driver":	"GTiff",
-	"height":	out_image.shape[1],
-	"width":	out_image.shape[2],
-	"transform":	out_transform
-})
-
-OUTPUT_FILE = os.path.join(TEMP_FOLDER, "cropped.tif")
-
-with rasterio.open(OUTPUT_FILE, "w", **out_meta) as dest:
-    dest.write(out_image)
-'''
+    logger.info('END')
