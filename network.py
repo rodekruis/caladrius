6cd00b2c26f6e29adc64912e5ac9cac8e32fbf3e
samarthbhargav <samarth.bhargav92@gmail.com>
2018-11-17 19:27:36 +0100

    It's time for bed

--- network.py
+++ network.py
@@ -162,9 +162,8 @@ class QuasiSiameseNetwork(object):
 
             with torch.set_grad_enabled(phase == "train"):
                 outputs = self.model(image1, image2)
-                #_, preds = torch.max(outputs, 1)
+                _, preds = torch.max(outputs, 1)
                 _, labels = torch.max(labels, 1)
-                print(outputs, labels)
                 loss = self.criterion(outputs, labels)
 
                 if phase == 'train':
