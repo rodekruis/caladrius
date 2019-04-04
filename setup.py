9d1b322cbb53f35265d8bff21e1bead904487ccf
turnerm <turnermon@gmail.com>
2019-04-04 17:13:33 +0200

    Added package installation and entrypoints

--- setup.py
+++ setup.py
@@ -6,4 +6,15 @@ with open('VERSION') as version_file:
 setup(
     name='caladrius',
     version=version,
+    url="https://github.com/rodekruis/caladrius",
+    packages=["caladrius"],
+    setup_requires=[],
+    tests_require=[],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'caladrius_run_nn=caladrius.run:main',
+            'caladrius_setup_data=caladrius.sint_maarten_2017:main',
+        ]
+    }
 )
