68759a19c99a7ca650b547872878d782df598d2b
turnerm <turnermon@gmail.com>
2019-06-11 11:17:30 +0200

    Added colours

--- caladrius/interface/src/scatterplot.js
+++ caladrius/interface/src/scatterplot.js
@@ -2,6 +2,9 @@ import React from 'react';
 import * as d3 from "d3";
 import load_csv_data from './data.js';
 
+
+const margins = { 'top': 50, 'right': 50, 'bottom': 50, 'left': 50 };
+
 class ScatterPlot extends React.Component {
   constructor(props) {
     super(props)
@@ -30,20 +33,40 @@ class ScatterPlot extends React.Component {
 
  updateChart() {
 
-    let xScale = d3.scaleLinear().domain([0, 1]).range([0, this.props.width])
-    let yScale = d3.scaleLinear().domain([0, 1]).range([0, this.props.height])
+    let width = this.props.width - margins.left - margins.right
+    let height = this.props.height - margins.top - margins.bottom
+    let xScale = d3.scaleLinear().domain([0, 1]).range([0, width])
+    let yScale = d3.scaleLinear().domain([0, 1]).range([height, 0])
+    let inverseXScale = d3.scaleLinear().domain([0, width]).range([0,1])
 
     this.state.data.then(data => {
+
         let u = d3.select(this.svgEl)
           .selectAll('circle')
           .data(data)
 
         u.enter()
+          .append("g")
+            .attr("class", "plot-space")
+            .attr("transform",
+      	        "translate(" + margins.left + "," + margins.top + ")"
+            )
+
           .append('circle')
-          .merge(u)
-          .attr('cx', d => xScale(d.label))
-          .attr('cy', d => yScale(d.prediction))
+          .attr('cx', d => xScale(d.prediction))
+          .attr('cy', d => yScale(d.label))
           .attr('r', d => 5)
+          .style("fill", function(d){
+            if (inverseXScale(d3.select(this).attr('cx')) < 0.3) {
+                return 'orange'
+            } else if (inverseXScale(d3.select(this).attr('cx')) > 0.7) {
+                return 'steelBlue'
+            } else {
+                return 'purple'
+            }
+          })
+          .call(d3.axisBottom(xScale))
+          .call(d3.axisLeft(yScale))
 
         u.exit().remove()
     })
