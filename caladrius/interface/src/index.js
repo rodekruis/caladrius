8cb24e2ae09ff3c19a166c0d91589f0c3b711f3a
turnerm <turnermon@gmail.com>
2019-06-11 09:33:25 +0200

    Main part of react app

---
+++ caladrius/interface/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ScatterPlot from './scatterplot.js';
+
+ReactDOM.render(
+  <ScatterPlot width={800} height={600} />,
+  document.getElementById('root')
+)
