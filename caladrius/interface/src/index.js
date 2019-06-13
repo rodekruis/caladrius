184701c4b2933594d34609601e205685faea6fa1
turnerm <turnermon@gmail.com>
2019-06-13 17:35:33 +0200

    Added table

--- caladrius/interface/src/index.js
+++ caladrius/interface/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ScatterPlot from './ScatterPlot.js';
+import FullDataTable from './Table.js';
 
 ReactDOM.render(
-  <ScatterPlot width={800} height={600} />,
+  <div>
+    <ScatterPlot width={800} height={600} />
+    <FullDataTable/>
+  </div>,
   document.getElementById('root')
-)
+);
