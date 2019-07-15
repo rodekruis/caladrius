5bad52245c0f86f950b6c7e0b31a47a0d8f2de53
turnerm <turnermon@gmail.com>
2019-07-15 02:11:29 +0200

    Added heatmap

--- caladrius/interface/src/Map.js
+++ caladrius/interface/src/Map.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
+import { Map as LeafletMap, TileLayer, Polygon, LayerGroup, LayersControl } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer'
 import 'leaflet/dist/leaflet.css';
-import * as L from 'leaflet';
-import { Map as LeafletMap, TileLayer, Polygon, LayerGroup} from 'react-leaflet';
-import { get_point_colour, selected } from './colours';
-import 'leaflet.heat';
-
+import { get_point_colour } from './colours';
 
 const map_url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors";
@@ -12,8 +10,28 @@ const zoom = 18
 
 export class Map extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.get_polygon_array = this.get_polygon_array.bind(this);
+  }
+
   render () {
- 
+    return (
+      <LeafletMap center={this.props.map_center} zoom={zoom} style={{height: this.props.height}}>
+        <TileLayer url={map_url} attribution={attribution} />
+         <LayerGroup>
+         	{this.get_polygon_array()}
+        </LayerGroup>
+       <LayersControl>        
+          {heatMapMaker(this.props.data, 'label')}
+          {heatMapMaker(this.props.data, 'prediction')}
+          {heatMapMaker(this.props.data, 'category')}
+        </LayersControl>
+    </LeafletMap>
+    )
+  }
+
+  get_polygon_array() {
     let polygonArray = this.props.data.map(datum => {
       let colour = get_point_colour(datum.prediction, 
         this.props.damage_boundary_a, this.props.damage_boundary_b,
@@ -27,57 +45,50 @@ export class Map extends React.Component {
        />
       );
     });
-
-    return (
-      <LeafletMap center={this.props.map_center} zoom={zoom} style={{height: this.props.height}}>
-        <TileLayer url={map_url} attribution={attribution} />
-         <LayerGroup>
-         	{polygonArray}
-        </LayerGroup>
-     </LeafletMap>
-    )
-  }
+    return polygonArray
 }
 
-function openWeatherMapo(layer) {
-  return L.tileLayer('https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid={api_key}', {
-   layer: layer,
-   maxZoom:17,
-   api_key: '2733c9a9c041a4ba7ce1963ae1a97dd4'
-   })
 }
 
-function heatMapMaker(mymap, cacheData, mode) {
+
+function heatMapMaker(cacheData, mode) {
   let heatCoordinates = [];
   if (mode === "label") {
     heatCoordinates = cacheData.map(
         x => [
-            x.feature.geometry.coordinates[0][0][0][1],
             x.feature.geometry.coordinates[0][0][0][0],
+            x.feature.geometry.coordinates[0][0][0][1],
             x.label
         ]
     )
   } else if (mode === "prediction") {
     heatCoordinates = cacheData.map(
         x => [
-            x.feature.geometry.coordinates[0][0][0][1],
             x.feature.geometry.coordinates[0][0][0][0],
+            x.feature.geometry.coordinates[0][0][0][1],
             x.prediction
         ]
     )
   } else if (mode === "category") {
     heatCoordinates = cacheData.map(
         x => [
-            x.feature.geometry.coordinates[0][0][0][1],
             x.feature.geometry.coordinates[0][0][0][0],
+            x.feature.geometry.coordinates[0][0][0][1],
             categoryToValue(x.feature.properties._damage)
         ]
     )
   }
-  // https://github.com/Leaflet/Leaflet.heat
-  return L.heatLayer(heatCoordinates, {radius: 30, blur: 15 });
+  return (  
+    <LayersControl.Overlay name={mode}>
+      <HeatmapLayer
+            points={heatCoordinates}
+            longitudeExtractor={m => m[1]}
+            latitudeExtractor={m => m[0]}
+            intensityExtractor={m => parseFloat(m[2])}
+        />
+    </LayersControl.Overlay>
+  )
 }
-
 
 function categoryToValue(category) {
   var value
