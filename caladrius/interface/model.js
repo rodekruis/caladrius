6b785c1f167359a67441dc4f7b70d52638cdfcf2
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2019-10-14 16:29:39 +0200

    handle folder not found exception

--- caladrius/interface/model.js
+++ caladrius/interface/model.js
@@ -11,40 +11,47 @@ class ModelManager {
     get_models() {
         return new Promise((resolve, reject) => {
             fs.readdir(MODEL_DIRECTORY, (err, models) => {
-                if (err) reject(null);
-                const promises = [];
-                models.forEach(model => {
-                    const model_path = path.join(MODEL_DIRECTORY, model, 'predictions');
-                    promises.push(
-                        new Promise((resolve_, reject_) => {
-                            return fs.readdir(model_path, (err, filenames) => {
-                                if(!err && filenames.length > 0) {
-                                    const model_split = model.split('-');
-                                    const test_predictions_filenames = filenames.filter(
-                                        filename => filename.includes('test'));
-                                    const validation_predictions_filenames = filenames.filter(
-                                        filename => filename.includes('validation'));
-                                    resolve_({
-                                        model_name: model_split[0],
-                                        model_directory: model,
-                                        input_size: parseInt(model_split[1].split('_').slice(-1).pop()),
-                                        learning_rate: parseFloat(model_split[2].split('_').slice(-1).pop()),
-                                        batch_size: parseInt(model_split[3].split('_').slice(-1).pop()),
-                                        predictions: {
-                                            test: test_predictions_filenames,
-                                            validation: validation_predictions_filenames
-                                        }
-                                    });
-                                } else {
-                                    resolve_(null);
-                                }
-                            });
-                        })
-                    );
-                });
-                Promise.all(promises).then(models => {
-                    resolve(models.filter(model => model && model.predictions.test.length > 0));
-                });
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        resolve([]);
+                    } else {
+                        reject(null);
+                    }
+                } else {
+                    const promises = [];
+                    models.forEach(model => {
+                        const model_path = path.join(MODEL_DIRECTORY, model, 'predictions');
+                        promises.push(
+                            new Promise((resolve_, reject_) => {
+                                return fs.readdir(model_path, (err, filenames) => {
+                                    if(!err && filenames.length > 0) {
+                                        const model_split = model.split('-');
+                                        const test_predictions_filenames = filenames.filter(
+                                            filename => filename.includes('test'));
+                                        const validation_predictions_filenames = filenames.filter(
+                                            filename => filename.includes('validation'));
+                                        resolve_({
+                                            model_name: model_split[0],
+                                            model_directory: model,
+                                            input_size: parseInt(model_split[1].split('_').slice(-1).pop()),
+                                            learning_rate: parseFloat(model_split[2].split('_').slice(-1).pop()),
+                                            batch_size: parseInt(model_split[3].split('_').slice(-1).pop()),
+                                            predictions: {
+                                                test: test_predictions_filenames,
+                                                validation: validation_predictions_filenames
+                                            }
+                                        });
+                                    } else {
+                                        resolve_(null);
+                                    }
+                                });
+                            })
+                        );
+                    });
+                    Promise.all(promises).then(models => {
+                        resolve(models.filter(model => model && model.predictions.test.length > 0));
+                    });
+                }
             });
         });
     }
