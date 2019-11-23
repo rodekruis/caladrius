295e314322f8d28e68fec1b1bdf8937ad8e5b23f
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2019-11-23 15:44:33 +0100

    bulma app loading

--- caladrius/interface/client/src/App.js
+++ caladrius/interface/client/src/App.js
@@ -15,6 +15,7 @@ export class App extends React.Component {
             data: [],
             selected_datum: {},
             admin_regions: [],
+            loading: true,
         };
         this.load_model = this.load_model.bind(this);
         this.set_datum = this.set_datum.bind(this);
@@ -23,20 +24,28 @@ export class App extends React.Component {
 
     componentDidMount() {
         load_admin_regions(admin_regions => {
-            this.setState({ admin_regions: admin_regions });
+            fetch("/api/models")
+                .then(res => res.json())
+                .then(models => {
+                    this.setState({
+                        admin_regions: admin_regions,
+                        models: models,
+                        loading: false,
+                    });
+                });
         });
-        fetch("/api/models")
-            .then(res => res.json())
-            .then(models => {
-                this.setState({ models: models });
-            });
     }
 
     load_model(model) {
+        this.setState({ loading: true });
         const model_name = model.model_directory;
         const prediction_filename = model.predictions.test[0];
         load_csv_data(model_name, prediction_filename, data => {
-            this.setState({ selected_model: model, data: data });
+            this.setState({
+                selected_model: model,
+                data: data,
+                loading: false,
+            });
         });
     }
 
@@ -63,14 +72,31 @@ export class App extends React.Component {
                     render_model_selector={this.render_model_selector}
                     data={this.state.data}
                 />
-                <Dashboard
-                    data={this.state.data}
-                    selected_datum={this.state.selected_datum}
-                    admin_regions={this.state.admin_regions}
-                    set_datum={this.set_datum}
-                    render_model_selector={this.render_model_selector}
-                    selected_model={this.state.selected_model}
-                />
+                {this.state.loading ? (
+                    <section className="hero is-large">
+                        <div className="hero-body">
+                            <div className="container">
+                                <h2 className="title">
+                                    <progress
+                                        class="progress is-warning"
+                                        max="100"
+                                    >
+                                        100%
+                                    </progress>
+                                </h2>
+                            </div>
+                        </div>
+                    </section>
+                ) : (
+                    <Dashboard
+                        data={this.state.data}
+                        selected_datum={this.state.selected_datum}
+                        admin_regions={this.state.admin_regions}
+                        set_datum={this.set_datum}
+                        render_model_selector={this.render_model_selector}
+                        selected_model={this.state.selected_model}
+                    />
+                )}
                 <AddressList
                     data={this.state.data}
                     view_datapoint={this.set_datum}
