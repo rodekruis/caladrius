a42ff150b1402bda13b623873491c175e01d4ece
Tinka <tinka.valentijn@aalto.fi>
2019-12-10 10:14:22 +0100

    comment the workings of the network

--- caladrius/model/network.py
+++ caladrius/model/network.py
@@ -14,11 +14,19 @@ logger = create_logger(__name__)
 
 
 def get_pretrained_iv3(output_size):
+    """
+    Get the pretrained Inception_v3 model, and change it for our use
+    Args:
+        output_size (int): Size of the output of the last layer
+
+    Returns:
+        model_conv: Model with Inception_v3 as base
+    """
     #fetch pretrained inception_v3 model
     model_conv = torchvision.models.inception_v3(pretrained=True)
 
     #requires_grad indicates if parameter is learnable
-    #so set all parameters to non-learnable
+    #so here set all parameters to non-learnable
     for i, param in model_conv.named_parameters():
         param.requires_grad = False
 
@@ -28,6 +36,9 @@ def get_pretrained_iv3(output_size):
     #creaty fully connected layer
     model_conv.fc = nn.Linear(num_ftrs, output_size)
 
+    #want almost all parameters learnable except for first few layers
+    #so here set most parameters to learnable
+    #idea is that first few layers learn types of features that are the same in all types of images --> don't have to retrain
     ct = []
     for name, child in model_conv.named_children():
         if "Conv2d_4a_3x3" in ct:
@@ -44,24 +55,38 @@ def get_pretrained_iv3(output_size):
 
 
 def get_pretrained_iv3_transforms(set_name):
+    """
+    Compose a series of image transformations to be performed on the input data
+    Args:
+        set_name (str): the dataset you want the transformations for. Can be "train","validation", and/or "test"
+
+    Returns:
+        Composition of transformations for given set name
+    """
     mean = [0.5, 0.5, 0.5]
     std = [0.5, 0.5, 0.5]
     scale = 360
     input_shape = 299
     train_transform = transforms.Compose(
         [
+            #resize every image to scale x scale pixels
             transforms.Resize(scale),
+            #crop every image to input_shape x input_shape pixels.
+            #This is needed for the inception model.
             transforms.RandomResizedCrop(input_shape),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.RandomRotation(degrees=90),
+            #converts image to type Torch and normalizes [0,1]
             transforms.ToTensor(),
+            #normalizes [-1,1]
             transforms.Normalize(mean, std),
         ]
     )
 
     test_transform = transforms.Compose(
         [
+            #for testing and validation we don't want any permutations of the image, solely cropping and normalizing
             transforms.Resize(scale),
             transforms.CenterCrop(input_shape),
             transforms.ToTensor(),
@@ -80,11 +105,19 @@ class SiameseNetwork(nn.Module):
     def __init__(
         self, output_size=512, similarity_layers_sizes=[512, 512], dropout=0.5
     ):
+        """
+        Construct the Siamese network
+        Args:
+            output_size (int): output size of the Inception v3 model
+            similarity_layers_sizes (list of ints): output sizes of each similarity layer
+            dropout (float): amount of dropout, same for each layer
+        """
         super().__init__()
         self.left_network = get_pretrained_iv3(output_size)
         self.right_network = get_pretrained_iv3(output_size)
 
         similarity_layers = OrderedDict()
+        #fully connected layer where input is concatenated features of the two inception models
         similarity_layers["layer_0"] = nn.Linear(
             output_size * 2, similarity_layers_sizes[0]
         )
@@ -93,6 +126,7 @@ class SiameseNetwork(nn.Module):
         if dropout:
             similarity_layers["dropout_0"] = nn.Dropout(dropout, inplace=True)
         prev_hidden_size = similarity_layers_sizes[0]
+        #make a hidden layer for each entry in similarity_layers_sizes
         for idx, hidden in enumerate(similarity_layers_sizes[1:], 1):
             similarity_layers["layer_{}".format(idx)] = nn.Linear(
                 prev_hidden_size, hidden
@@ -105,10 +139,19 @@ class SiameseNetwork(nn.Module):
                 )
 
         self.similarity = nn.Sequential(similarity_layers)
-
+        #final layer with one output which is the amount of damage from 0 to 1
         self.output = nn.Linear(hidden, 1)
 
     def forward(self, image_1, image_2):
+        """
+        Define the feedforward sequence
+        Args:
+            image_1: Image fed in to left network
+            image_2: Image fed in to right network
+
+        Returns:
+            Predicted output
+        """
         left_features = self.left_network(image_1)
         right_features = self.right_network(image_2)
 
