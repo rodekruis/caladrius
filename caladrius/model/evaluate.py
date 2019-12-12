f808cae1f9e454dba91d264b99b94e455a76fb3c
Tinka <tinka.valentijn@aalto.fi>
2019-12-12 14:52:24 +0100

    change f1 score to weighted average instead of micro

--- caladrius/model/evaluate.py
+++ caladrius/model/evaluate.py
@@ -25,52 +25,52 @@ class RollingEval(object):
         self.y_pred.extend(y_p.cpu().detach().numpy())
 
     def f1_score(self):
-        #look at different types of average. average="weighted" is used by xview2_baseline
-        return f1_score(self.y_true, self.y_pred, average="micro")
+        #look at different types of average. average="weighted" is used by xview2_baseline. Averaged="micro" was used by caladrius
+        return f1_score(self.y_true, self.y_pred, average="weighted")
 
     def recall(self):
-        return recall_score(self.y_true, self.y_pred, average="micro")
+        return recall_score(self.y_true, self.y_pred, average="weighted")
 
-    def every_measure(self):
-        return classification_report(self.y_true, self.y_pred)
+    # def every_measure(self):
+    #     return classification_report(self.y_true, self.y_pred)
 
 
-class Evaluator(object):
-    def __init__(self, model, sets):
-
-        self.model = model
-        # sets is a dictionary (set_name -> dataloader)
-        self.datasets = sets
-
-    def evaluate_set(self, set_name):
-        assert set_name in self.datasets.keys()
-        logger.info("Starting evaluation of model")
-        y_true, y_pred = self.gather_outputs(self.model, self.datasets[set_name])
-        micro_f1_score = f1_score(y_true, y_pred, average="micro")
-
-        logger.info("F1-Score ({}) : {}".format(set_name, micro_f1_score))
-        return micro_f1_score
-
-    def gather_outputs(self, model, dataset):
-        y_true = []
-        y_pred = []
-        logger.info(
-            "Gathering inputs. Total number of datapoints: {}".format(len(dataset))
-        )
-        with torch.no_grad():
-            for idx, (image1, image2, y_true_batch) in enumerate(dataset):
-                y_pred_batch = model(image1, image2)
-
-                y_pred_batch = y_pred_batch.argmax(1)
-                y_pred_batch.extend(y_pred_batch.detach().numpy())
-                y_true.extend(y_true_batch.detach().numpy())
-
-        return y_true, y_pred
-
-    def evaluate(self, results_path):
-        results = {}
-        for set_name, dataset in self.datasets.items():
-            results[set_name] = self.evaluate_set(set_name)
-
-        with open(results_path, "w") as writer:
-            json.dump(writer, results, indent=2)
+# class Evaluator(object):
+#     def __init__(self, model, sets):
+#
+#         self.model = model
+#         # sets is a dictionary (set_name -> dataloader)
+#         self.datasets = sets
+#
+#     def evaluate_set(self, set_name):
+#         assert set_name in self.datasets.keys()
+#         logger.info("Starting evaluation of model")
+#         y_true, y_pred = self.gather_outputs(self.model, self.datasets[set_name])
+#         micro_f1_score = f1_score(y_true, y_pred, average="micro")
+#
+#         logger.info("F1-Score ({}) : {}".format(set_name, micro_f1_score))
+#         return micro_f1_score
+#
+#     def gather_outputs(self, model, dataset):
+#         y_true = []
+#         y_pred = []
+#         logger.info(
+#             "Gathering inputs. Total number of datapoints: {}".format(len(dataset))
+#         )
+#         with torch.no_grad():
+#             for idx, (image1, image2, y_true_batch) in enumerate(dataset):
+#                 y_pred_batch = model(image1, image2)
+#
+#                 y_pred_batch = y_pred_batch.argmax(1)
+#                 y_pred_batch.extend(y_pred_batch.detach().numpy())
+#                 y_true.extend(y_true_batch.detach().numpy())
+#
+#         return y_true, y_pred
+#
+#     def evaluate(self, results_path):
+#         results = {}
+#         for set_name, dataset in self.datasets.items():
+#             results[set_name] = self.evaluate_set(set_name)
+#
+#         with open(results_path, "w") as writer:
+#             json.dump(writer, results, indent=2)
