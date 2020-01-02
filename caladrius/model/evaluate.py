report all metrics - accuracy, correct, total, precision, recall and f1 - both micro and macro, switch between model types, added a lighter siamese network

caladrius/model/evaluate.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 import torch
 
 from utils import create_logger
@@ -21,6 +21,16 @@ class RollingEval(object):
     def add(self, labels, predictions, loss):
         self.labels = self.labels.to(labels.device)
         self.predictions = self.predictions.to(predictions.device)
+        if self.output_type == "regression":
+            self.labels = self.labels.float()
+            self.predictions = self.predictions.float()
+            labels = labels.float()
+            predictions = predictions.float()
+        else:
+            self.labels = self.labels.long()
+            self.predictions = self.predictions.long()
+            labels = labels.long()
+            predictions = predictions.long()
         self.labels = torch.cat([self.labels, labels], dim=0)
         self.predictions = torch.cat([self.predictions, predictions], dim=0)
         self.total_loss += loss * predictions.size(0)
@@ -40,14 +50,29 @@ class RollingEval(object):
         if self.output_type == "regression":
             labels = self.to_classes(labels)
             predictions = self.to_classes(predictions)
-        return self.f1_score(labels, predictions)
+        return self.precision_recall_fscore_support_accuracy(labels, predictions)
 
     def loss(self):
         return self.total_loss / self.predictions.size(0)
 
-    def f1_score(self, labels, predictions):
+    def precision_recall_fscore_support_accuracy(self, labels, predictions):
         # average="weighted" is used by xview2_baseline
         # average="micro" was used by caladrius
-        return f1_score(
-            labels.cpu().detach(), predictions.cpu().detach(), average="macro"
+        labels = labels.cpu().detach()
+        predictions = predictions.cpu().detach()
+        accuracy_value = accuracy_score(labels, predictions, normalize=True)
+        number_of_correct = accuracy_score(labels, predictions, normalize=False)
+        total_number = len(predictions)
+        micro_precision_recall_fscore_support_values = precision_recall_fscore_support(
+            labels, predictions, average="micro"
+        )
+        macro_precision_recall_fscore_support_values = precision_recall_fscore_support(
+            labels, predictions, average="macro"
+        )
+        return (
+            accuracy_value,
+            number_of_correct,
+            total_number,
+            micro_precision_recall_fscore_support_values,
+            macro_precision_recall_fscore_support_values,
         )

caladrius/model/networks/inception_siamese_network.py
@@ -102,7 +102,7 @@ def get_pretrained_iv3_transforms(set_name):
     }[set_name]
 
 
-class SiameseNetwork(nn.Module):
+class InceptionSiameseNetwork(nn.Module):
     def __init__(
         self,
         output_size=512,

caladrius/model/trainer.py
@@ -10,7 +10,14 @@ import torchvision.transforms as transforms
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.tensorboard import SummaryWriter
 
-from model.network import get_pretrained_iv3_transforms, SiameseNetwork
+from model.networks.inception_siamese_network import (
+    get_pretrained_iv3_transforms,
+    InceptionSiameseNetwork,
+)
+from model.networks.light_siamese_network import (
+    get_light_siamese_transforms,
+    LightSiameseNetwork,
+)
 from utils import create_logger, readable_float, dynamic_report_key
 from model.evaluate import RollingEval
 
@@ -26,14 +33,20 @@ class QuasiSiameseNetwork(object):
         self.lr = args.learning_rate
         self.output_type = args.output_type
 
+        network_architecture_class = LightSiameseNetwork
+        network_architecture_transforms = get_light_siamese_transforms
+        if args.model_type == "inception":
+            network_architecture_class = InceptionSiameseNetwork
+            network_architecture_transforms = get_pretrained_iv3_transforms
+
         # define the loss measure
         if self.output_type == "regression":
             self.criterion = nnloss.MSELoss()
-            self.model = SiameseNetwork()
+            self.model = network_architecture_class()
         elif self.output_type == "classification":
             self.criterion = nnloss.CrossEntropyLoss()
             self.n_classes = 4  # replace by args
-            self.model = SiameseNetwork(
+            self.model = network_architecture_class(
                 output_type=self.output_type, n_classes=self.n_classes
             )
 
@@ -44,7 +57,7 @@ class QuasiSiameseNetwork(object):
             self.model = torch.nn.DataParallel(self.model)
 
         for s in ("train", "validation", "test", "inference"):
-            self.transforms[s] = get_pretrained_iv3_transforms(s)
+            self.transforms[s] = network_architecture_transforms(s)
 
         logger.debug("Num params: {}".format(len([_ for _ in self.model.parameters()])))
 
@@ -60,6 +73,8 @@ class QuasiSiameseNetwork(object):
         self.model_path = args.model_path
         self.prediction_path = args.prediction_path
         self.model_type = args.model_type
+        self.is_statistical_model = args.statistical_model
+        self.is_neural_model = args.neural_model
         self.log_step = args.log_step
 
     def get_random_output_values(self, output_shape):
@@ -94,7 +109,7 @@ class QuasiSiameseNetwork(object):
     def get_outputs_preds(
         self, image1, image2, random_target_shape, average_target_size
     ):
-        if self.model_type == "siamese":
+        if self.is_neural_model:
             outputs = self.model(image1, image2).squeeze()
         elif self.model_type == "random":
             output_shape = (
@@ -126,7 +141,7 @@ class QuasiSiameseNetwork(object):
 
         Returns:
             epoch_loss (float): loss of this epoch
-            epoch_accuracy (float): accuracy of this epoch
+            epoch_score (float): micro recall of this epoch
         """
         assert phase in ("train", "validation", "test")
 
@@ -181,27 +196,67 @@ class QuasiSiameseNetwork(object):
 
             if idx % self.log_step == 0:
                 logger.debug(
-                    "Epoch: {:03d} Phase: {:10s} Batch {:04d}/{:04d}: Loss: {:.4f} Score: {:.4f}".format(
-                        epoch, phase, idx, len(loader), batch_loss, batch_score
+                    "Epoch: {:03d} Phase: {:10s} Batch {:04d}/{:04d}: Loss: {:.4f} Accuracy: {:.4f} Correct: {:d} Total: {:d}".format(
+                        epoch,
+                        phase,
+                        idx,
+                        len(loader),
+                        batch_loss,
+                        batch_score[0],
+                        batch_score[1],
+                        batch_score[2],
+                    )
+                )
+                logger.debug(
+                    "Epoch: {:03d} Phase: {:10s} Batch {:04d}/{:04d}: (Micro) Precision: {:.4f} Recall: {:.4f} F1: {:.4f}".format(
+                        epoch,
+                        phase,
+                        idx,
+                        len(loader),
+                        batch_score[3][0],
+                        batch_score[3][1],
+                        batch_score[3][2],
+                    )
+                )
+                logger.debug(
+                    "Epoch: {:03d} Phase: {:10s} Batch {:04d}/{:04d}: (Macro) Precision: {:.4f} Recall: {:.4f} F1: {:.4f}".format(
+                        epoch,
+                        phase,
+                        idx,
+                        len(loader),
+                        batch_score[4][0],
+                        batch_score[4][1],
+                        batch_score[4][2],
                     )
                 )
 
         epoch_loss = rolling_eval.loss()
         epoch_score = rolling_eval.score()
+        epoch_main_metric = epoch_score[3][1]
 
         if not (phase == "train"):
             prediction_file.write(
-                "Epoch {:03d} Score: {:.4f}\n".format(epoch, epoch_score)
+                "Epoch {:03d} (Micro) Recall: {:.4f}\n".format(epoch, epoch_main_metric)
             )
             prediction_file.close()
 
         logger.info(
-            "Epoch {:03d} Phase: {:10s} Loss: {:.4f} Score: {:.4f}".format(
-                epoch, phase, epoch_loss, epoch_score
+            "Epoch {:03d} Phase: {:10s}: Loss: {:.4f} Accuracy: {:.4f} Correct: {:d} Total: {:d}".format(
+                epoch, phase, epoch_loss, epoch_score[0], epoch_score[1], epoch_score[2]
+            )
+        )
+        logger.info(
+            "Epoch {:03d} Phase: {:10s}: (Micro) Precision: {:.4f} Recall: {:.4f} F1: {:.4f}".format(
+                epoch, phase, epoch_score[3][0], epoch_score[3][1], epoch_score[3][2]
+            )
+        )
+        logger.info(
+            "Epoch {:03d} Phase: {:10s}: (Macro) Precision: {:.4f} Recall: {:.4f} F1: {:.4f}".format(
+                epoch, phase, epoch_score[4][0], epoch_score[4][1], epoch_score[4][2]
             )
         )
 
-        return epoch_loss, epoch_score
+        return epoch_loss, epoch_main_metric
 
     def train(self, run_report, datasets, number_of_epochs):
         """
@@ -217,41 +272,42 @@ class QuasiSiameseNetwork(object):
         train_set, train_loader = datasets.load("train")
         validation_set, validation_loader = datasets.load("validation")
 
-        best_accuracy, best_model_wts = 0.0, copy.deepcopy(self.model.state_dict())
+        best_validation_score, best_model_wts = (
+            0.0,
+            copy.deepcopy(self.model.state_dict()),
+        )
 
         start_time = time.time()
         run_report.train_start_time = (
             datetime.utcnow().replace(microsecond=0).isoformat()
         )
         run_report.train_loss = []
-        run_report.train_accuracy = []
+        run_report.train_score = []
         run_report.validation_loss = []
-        run_report.validation_accuracy = []
+        run_report.validation_score = []
 
         for epoch in range(1, number_of_epochs + 1):
             # train network
-            train_loss, train_accuracy = self.run_epoch(
-                epoch, train_loader, phase="train"
-            )
+            train_loss, train_score = self.run_epoch(epoch, train_loader, phase="train")
             run_report.train_loss.append(readable_float(train_loss))
-            run_report.train_accuracy.append(readable_float(train_accuracy))
+            run_report.train_score.append(readable_float(train_score))
 
             # eval on validation
-            validation_loss, validation_accuracy = self.run_epoch(
+            validation_loss, validation_score = self.run_epoch(
                 epoch, validation_loader, phase="validation"
             )
             run_report.validation_loss.append(readable_float(validation_loss))
-            run_report.validation_accuracy.append(readable_float(validation_accuracy))
+            run_report.validation_score.append(readable_float(validation_score))
 
             self.writer.add_scalar("Train/Loss", train_loss, epoch)
-            self.writer.add_scalar("Train/Accuracy", train_accuracy, epoch)
+            self.writer.add_scalar("Train/Score", train_score, epoch)
             self.writer.add_scalar("Validation/Loss", validation_loss, epoch)
-            self.writer.add_scalar("Validation/Accuracy", validation_accuracy, epoch)
+            self.writer.add_scalar("Validation/Score", validation_score, epoch)
 
             self.lr_scheduler.step(validation_loss)
 
-            if validation_accuracy > best_accuracy:
-                best_accuracy = validation_accuracy
+            if validation_score > best_validation_score:
+                best_validation_score = validation_score
                 best_model_wts = copy.deepcopy(self.model.state_dict())
 
                 logger.info(
@@ -270,7 +326,7 @@ class QuasiSiameseNetwork(object):
 
         logger.info("Training complete in {}".format(run_report.train_duration))
 
-        logger.info("Best validation Accuracy: {:4f}.".format(best_accuracy))
+        logger.info("Best validation score: {:4f}.".format(best_validation_score))
         return run_report
 
     def test(self, run_report, datasets):
@@ -283,8 +339,7 @@ class QuasiSiameseNetwork(object):
         Returns:
             run_report (dict): configuration parameters for testing with testing statistics
         """
-        is_statistical_model = self.model_type != "siamese"
-        if is_statistical_model:
+        if self.is_statistical_model:
             train_set, _ = datasets.load("train")
         else:
             self.model.load_state_dict(
@@ -293,38 +348,44 @@ class QuasiSiameseNetwork(object):
         test_set, test_loader = datasets.load("test")
         start_time = time.time()
         run_report[
-            dynamic_report_key("test_start_time", self.model_type, is_statistical_model)
+            dynamic_report_key(
+                "test_start_time", self.model_type, self.is_statistical_model
+            )
         ] = (datetime.utcnow().replace(microsecond=0).isoformat())
-        test_loss, test_accuracy = self.run_epoch(
+        test_loss, test_score = self.run_epoch(
             1,
             test_loader,
             phase="test",
-            train_set=train_set if is_statistical_model else None,
+            train_set=train_set if self.is_statistical_model else None,
         )
         run_report[
-            dynamic_report_key("test_loss", self.model_type, is_statistical_model)
+            dynamic_report_key("test_loss", self.model_type, self.is_statistical_model)
         ] = readable_float(test_loss)
         run_report[
-            dynamic_report_key("test_accuracy", self.model_type, is_statistical_model)
-        ] = readable_float(test_accuracy)
+            dynamic_report_key("test_score", self.model_type, self.is_statistical_model)
+        ] = readable_float(test_score)
         time_elapsed = time.time() - start_time
         run_report[
-            dynamic_report_key("test_end_time", self.model_type, is_statistical_model)
+            dynamic_report_key(
+                "test_end_time", self.model_type, self.is_statistical_model
+            )
         ] = (datetime.utcnow().replace(microsecond=0).isoformat())
 
         run_report[
-            dynamic_report_key("test_duration", self.model_type, is_statistical_model)
+            dynamic_report_key(
+                "test_duration", self.model_type, self.is_statistical_model
+            )
         ] = "{:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60)
 
         run_report[
-            dynamic_report_key("test", self.model_type, is_statistical_model)
+            dynamic_report_key("test", self.model_type, self.is_statistical_model)
         ] = True
 
         logger.info(
             "Testing complete in {}".format(
                 run_report[
                     dynamic_report_key(
-                        "test_duration", self.model_type, is_statistical_model
+                        "test_duration", self.model_type, self.is_statistical_model
                     )
                 ]
             )
@@ -337,8 +398,7 @@ class QuasiSiameseNetwork(object):
         Args:
             datasets: DataSet object with datasets loaded
         """
-        is_statistical_model = self.model_type != "siamese"
-        if is_statistical_model:
+        if self.is_statistical_model:
             train_set, _ = datasets.load("train")
         else:
             self.model.load_state_dict(

caladrius/utils.py
@@ -9,6 +9,9 @@ import json
 
 import torch
 
+NEURAL_MODELS = ["inception", "light"]
+STATISTICAL_MODELS = ["average", "random"]
+
 # logging
 
 logging.getLogger("Fiona").setLevel(logging.ERROR)
@@ -113,8 +116,8 @@ def configuration():
     parser.add_argument(
         "--model-type",
         type=str,
-        default="siamese",
-        choices=["siamese", "random", "average"],
+        default=NEURAL_MODELS[0],
+        choices=NEURAL_MODELS + STATISTICAL_MODELS,
         help="type of model",
     )
 
@@ -207,6 +210,8 @@ def configuration():
     arg_vars["run_report_path"] = os.path.join(
         arg_vars["checkpoint_path"], "run_report.json"
     )
+    arg_vars["statistical_model"] = arg_vars["model_type"] in STATISTICAL_MODELS
+    arg_vars["neural_model"] = arg_vars["model_type"] in NEURAL_MODELS
 
     if torch.cuda.is_available() and not arg_vars["disable_cuda"]:
         arg_vars["device"] = torch.device("cuda:{}".format(arg_vars["cuda_device"]))