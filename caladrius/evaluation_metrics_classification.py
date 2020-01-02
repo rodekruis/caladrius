36daa616323c59b8446e6ca6fef735fabfb5be19
Tinka <tinka.valentijn@aalto.fi>
2020-01-02 12:31:03 +0100

    restructure how and which performance measures are saved
    change file structure with one general performance folder, focus on recall, add random and average tests

--- caladrius/evaluation_metrics_classification.py
+++ caladrius/evaluation_metrics_classification.py
@@ -1,63 +1,31 @@
 import pandas as pd
 import numpy as np
-# from collections import Counter
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 import argparse
+import os
 
-def tp_fn_fp_perclass(preds, labels, c):
-    """
-    Computes the number of TPs, FNs, FPs, between a prediction (x) and a target (y) for the desired class (c)
-    Args:
-        pred (np.ndarray): prediction
-        targ (np.ndarray): target
-        c (int): positive class
-    """
-    TP = np.logical_and(preds == c, labels == c).sum()
-    FN = np.logical_and(preds != c, labels == c).sum()
-    FP = np.logical_and(preds == c, labels != c).sum()
-    return [TP, FN, FP]
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
 
-def precision(TP,FP):
-    assert TP >= 0 and FP >= 0
-#     if TP ==0 and FP ==0:
-#         return 1
-    if TP == 0:
-        return 0
-    else:
-        return TP/(TP+FP)
-def recall(TP,FN):
-    assert TP>=0 and FN >=0
-#     if TP==0 and FN==0:
-#         return 1
-    if TP==0:
-        return 0
-    else:
-        return TP/(TP+FN)
-def f1_score(PR,RE):
-    return 2*((PR*RE)/(PR+RE+1e-6))
 
 def harmonic_score(scores):
-#     print(sum((c+1e-6)**-1 for c in scores))
-    return len(scores)/sum((c+1e-6)**-1 for c in scores)
+    """
+    Calculate the harmonic mean of a list of scores
+    Args:
+        scores (list): list of scores
 
-def harmonic_scores2(scores):
-    print(scores)
-    print(sum(c**-1 for c in scores))
-    print(sum(c for c in scores))
-    print(sum(c for c in scores)/len(scores))
-    return len(scores)/(sum(c**-1 for c in scores))
-
-def df1(self):
-    """ damage f1. Computed using harmonic mean of damage f1s """
-    harmonic_mean = lambda xs: len(xs) / sum((x+1e-6)**-1 for x in xs)
-    return harmonic_mean(self.df1s)
+    Returns:
+        harmonic mean of scores
+    """
+    return len(scores) / sum((c + 1e-6) ** -1 for c in scores)
 
 
-def cm_analysis(y_true, y_pred, filename, labels, ymap=None, figsize=(10,10)):
+def cm_analysis(y_true, y_pred, filename, labels, ymap=None, figsize=(10, 10)):
     """
     Generate matrix plot of confusion matrix with pretty annotations.
     The plot image is saved to disk.
@@ -89,18 +57,59 @@ def cm_analysis(y_true, y_pred, filename, labels, ymap=None, figsize=(10,10)):
             p = cm_perc[i, j]
             if i == j:
                 s = cm_sum[i]
-                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+                annot[i, j] = "%.1f%%\n%d/%d" % (p, c, s)
             elif c == 0:
-                annot[i, j] = ''
+                annot[i, j] = ""
             else:
-                annot[i, j] = '%.1f%%\n%d' % (p, c)
+                annot[i, j] = "%.1f%%\n%d" % (p, c)
     cm = pd.DataFrame(cm, index=labels, columns=labels)
-    cm.index.name = 'Actual'
-    cm.columns.name = 'Predicted'
+    cm.index.name = "Actual"
+    cm.columns.name = "Predicted"
     fig, ax = plt.subplots(figsize=figsize)
-    sns.heatmap(cm, annot=annot, fmt='', ax=ax)
-    # plt.show()
-    plt.savefig(filename,bbox_inches="tight")
+    sns.heatmap(cm, annot=annot, fmt="", ax=ax)
+    plt.savefig(filename, bbox_inches="tight")
+
+
+def gen_score_overview(preds_filename):
+    """
+    Generate a dataframe with several performance measures
+    Args:
+        preds_filename: name of file where predictions are saved
+
+    Returns:
+        score_overview (pd.DataFrame): dataframe with several performance measures
+        df_pred (pd.DataFrame): dataframe with the predictions and true labels
+    """
+    preds_file = open(preds_filename)
+    lines = preds_file.readlines()[1:-1]
+    pred_info = []
+    for l in lines:
+        pred_info.extend([l.rstrip().split(" ")])
+    df_pred = pd.DataFrame(pred_info, columns=["OBJECTID", "label", "pred"])
+    df_pred.label = df_pred.label.astype(int)
+    df_pred.pred = df_pred.pred.astype(int)
+
+    preds = np.array(df_pred.pred)
+    labels = np.array(df_pred.label)
+
+    report = classification_report(preds, labels, digits=3, output_dict=True)
+    score_overview = pd.DataFrame(report).transpose()
+    score_overview = score_overview.append(pd.Series(name="harmonized avg"))
+    score_overview.loc["harmonized avg", ["precision", "recall", "f1-score"]] = [
+        harmonic_score(r)
+        for i, r in score_overview.loc[
+            ["0", "1", "2", "3"], ["precision", "recall", "f1-score"]
+        ].T.iterrows()
+    ]
+
+    damage_mapping = {
+        "0": "No damage",
+        "1": "Minor damage",
+        "2": "Major damage",
+        "3": "Destroyed",
+    }
+    score_overview.rename(index=damage_mapping, inplace=True)
+    return score_overview, df_pred
 
 
 def main():
@@ -109,70 +118,135 @@ def main():
     )
 
     parser.add_argument(
-        "--run-name",
-        type=str,
-        required=True,
-        help="name to identify execution",
+        "--run-name", type=str, required=True, help="name to identify execution",
     )
 
     parser.add_argument(
         "--run-folder",
-        # required=True,
-        #default=os.path.join('../data', 'xBD'),
         metavar="Folder name of outputs of run",
         help="Full path to the directory with txt file with labels and predictions",
     )
 
     args = parser.parse_args()
     if not args.run_folder:
-        args.run_folder="{}-input_size_32-learning_rate_0.001-batch_size_32".format(
-        args.run_name
-    )
-    test_file_name="{}_test_epoch_001_predictions.txt".format(args.run_name)
-    preds_filename="../runs/{}/predictions/{}".format(args.run_folder,test_file_name)
-    output_path="../runs/{}/performance/".format(args.run_folder)
+        args.run_folder = "{}-input_size_32-learning_rate_0.001-batch_size_32".format(
+            args.run_name
+        )
 
-    preds_file=open(preds_filename)
-    lines=preds_file.readlines()[1:-1]
-    pred_info=[]
-    for l in lines:
-        pred_info.extend([l.rstrip().split(" ")])
-    df_pred=pd.DataFrame(pred_info,columns=["OBJECTID","label","pred"])
-    df_pred.label=df_pred.label.astype(int)
-    df_pred.pred=df_pred.pred.astype(int)
+    # define all file names and paths
+    test_file_name = "{}_test_epoch_001_predictions.txt".format(args.run_name)
+    preds_model = "../runs/{}/predictions/{}".format(args.run_folder, test_file_name)
+    preds_random = "{}_random.txt".format(preds_model[:-4])
+    preds_average = "{}_average.txt".format(preds_model[:-4])
+    output_path = "../performance/"
+    score_overviews_path = os.path.join(output_path, "score_overviews/")
+    confusion_matrices_path = os.path.join(output_path, "confusion_matrices/")
+    for p in [output_path, score_overviews_path, confusion_matrices_path]:
+        if not os.path.exists(p):
+            os.makedirs(p)
 
-    damage_mapping = {0: "No damage", 1: "Minor damage", 2: "Major damage", 3: "Destroyed"}
+    # parameters that should be written to overview file
+    scores_params = [
+        "unweighted_recall",
+        "weighted_recall",
+        "harmonized_recall",
+        "harmrecall_random",
+        "harmrecall_average",
+    ]
+    scores_dict = dict.fromkeys(scores_params)
 
-    preds = np.array(df_pred.pred)
-    labels = np.array(df_pred.label)
-    # scores_classes = {"recall": {}, "precision": {}, "f1": {}}
-    # for c in damage_mapping.keys():
-    #     tp, fn, fp = tp_fn_fp_perclass(preds, labels, c)
-    #     scores_classes["recall"][c] = recall(tp, fn)
-    #     scores_classes["precision"][c] = precision(tp, fp)
-    #     scores_classes["f1"][c] = f1_score(scores_classes["precision"][c], scores_classes["recall"][c])
-    #
-    # score_overview = pd.DataFrame(scores_classes).T
-    # score_overview.rename(columns=damage_mapping, inplace=True)
-    # score_overview["Total"] = [harmonic_score(r) for i, r in score_overview.iterrows()]
+    for preds_filename, preds_type in zip(
+        [preds_model, preds_random, preds_average], ["model", "random", "average"]
+    ):
+        # check if file for preds type exists
+        if os.path.exists(preds_filename):
+            # generate overview with performance measures
+            score_overview, df_pred = gen_score_overview(preds_filename)
+            score_overview.to_csv(
+                "{}{}_overview_{}.csv".format(
+                    score_overviews_path, args.run_name, preds_type
+                )
+            )
+            print(
+                "macro recall {}".format(preds_type),
+                score_overview.loc["macro avg", "recall"],
+            )
+            print(
+                "weighted recall {}".format(preds_type),
+                score_overview.loc["weighted avg", "recall"],
+            )
+            if preds_type == "model":
+                # generate and save confusion matrix
+                cm_analysis(
+                    df_pred.label,
+                    df_pred.pred,
+                    "{}{}_confusion".format(confusion_matrices_path, args.run_name),
+                    [0, 1, 2, 3],
+                    figsize=(9, 12),
+                )
+                # save overview params
+                scores_dict["unweighted_recall"] = score_overview.loc[
+                    "macro avg", "recall"
+                ]
+                scores_dict["weighted_recall"] = score_overview.loc[
+                    "weighted avg", "recall"
+                ]
+                scores_dict["harmonized_recall"] = score_overview.loc[
+                    "harmonized avg", "recall"
+                ]
+            if preds_type == "random":
+                scores_dict["harmrecall_random"] = score_overview.loc[
+                    "harmonized avg", "recall"
+                ]
+            if preds_type == "average":
+                scores_dict["harmrecall_average"] = score_overview.loc[
+                    "harmonized avg", "recall"
+                ]
+        else:
+            print("No predictions for prediction type {}".format(preds_type))
 
-    report = classification_report(preds, labels, digits=3,output_dict=True)
-    score_overview = pd.DataFrame(report).transpose()
-    damage_mapping = {'0': "No damage", '1': "Minor damage", '2': "Major damage", '3': "Destroyed"}
-    score_overview.rename(index=damage_mapping,inplace=True)
-    print("macro f1",score_overview.loc["macro avg","f1-score"])
-    print("weighted f1", score_overview.loc["weighted avg", "f1-score"])
+    # save parameters to overview file
+    # replace old values if line with same run_name already exists
+    allruns_overview_file_name = "allruns_scores.txt"
+    allruns_file_path = os.path.join(output_path, allruns_overview_file_name)
+    fh, abs_path = mkstemp()
+    replicate = False
+    with fdopen(fh, "w+") as new_file:
+        new_file.write(
+            "run_name,{}\n".format(
+                ",".join(str(item) for item in list(scores_dict.keys()))
+            )
+        )
+        if os.path.isfile(allruns_file_path):
+            with open(allruns_file_path) as old_file:
+                next(old_file)
+                for line in old_file:
+                    if args.run_name in line:
+                        replicate = True
+                        new_file.write(
+                            "{},{}\n".format(
+                                args.run_name,
+                                ",".join(
+                                    "{:.3f}".format(item)
+                                    for item in list(scores_dict.values())
+                                ),
+                            )
+                        )
+                    else:
+                        new_file.write(line)
+        if not replicate:
+            new_file.write(
+                "{},{}\n".format(
+                    args.run_name,
+                    ",".join(
+                        "{:.3f}".format(item) for item in list(scores_dict.values())
+                    ),
+                )
+            )
+    if os.path.isfile(allruns_file_path):
+        remove(allruns_file_path)
+    move(abs_path, allruns_file_path)
 
-    score_overview.to_csv("{}score_overview.csv".format(output_path))
 
-    # energy.rename(index={'Republic of Korea': 'South Korea'}, inplace=True)
-
-    cm_analysis(df_pred.label, df_pred.pred, "{}confusion_matrix".format(output_path), [0, 1, 2, 3],figsize=(9,12))
-
-
-
-    # print(score_overview)
-    # print(harmonic_scores2(np.array(score_overview.loc["f1",["No damage","Minor damage","Major damage","Destroyed"]])))
-    # print(harmonic_scores2(np.array([0.5,0.25])))
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
