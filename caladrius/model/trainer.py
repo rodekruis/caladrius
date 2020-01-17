997ba9fc06806c631c5390fb74216c14e30b701f
Tinka <tinka.valentijn@aalto.fi>
2020-01-17 13:33:18 +0100

    add loading model when using model type probability

--- caladrius/model/trainer.py
+++ caladrius/model/trainer.py
@@ -152,9 +152,9 @@ class QuasiSiameseNetwork(object):
                     outputs = self.model(image1, image2).squeeze()
                     # print("model output",self.model(image1, image2))
                     # print("model output squeezed",self.model(image1, image2).squeeze())
-                    outputs_probability = nn.functional.softmax(
-                        self.model(image1, image2)
-                    ).squeeze()
+                    # outputs_probability = nn.functional.softmax(
+                    #     self.model(image1, image2)
+                    # ).squeeze()
                     # print("outputs probability",outputs_probability)
                 elif model_type == "random":
                     if self.output_type == "regression":
@@ -183,6 +183,7 @@ class QuasiSiameseNetwork(object):
 
                 # if not (phase == "train"):
                 if model_type != "probability":
+                    print("preds view", preds.view(-1).tolist())
                     prediction_file.writelines(
                         [
                             "{} {} {}\n".format(*line)
@@ -194,7 +195,13 @@ class QuasiSiameseNetwork(object):
                         ]
                     )
                 else:
-                    output_probability.extend(outputs_probability.tolist())
+
+                    # print("outputs probability",outputs_probability)
+                    # _, preds_prob = torch.max(outputs_probability, 1)
+                    # print("preds prob",preds_prob)
+                    # print("preds",preds)
+                    print("preds view", preds.view(-1).tolist())
+                    output_probability.extend(outputs.tolist())  # outputs_probability
                 rolling_eval.add(labels, preds)
 
             running_loss += loss.item() * image1.size(0)
@@ -333,7 +340,7 @@ class QuasiSiameseNetwork(object):
             predictions_path (str): path to write predictions to
             model_type: type of model
         """
-        if model_type == "quasi-siamese":
+        if model_type == "quasi-siamese" or model_type == "probability":
             self.model.load_state_dict(torch.load(model_path, map_location=device))
         test_set, test_loader = datasets.load("test")
         self.run_epoch(
