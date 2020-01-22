2b9c5d6351d60bb188ba304dd305c6e82f0d2fb8
Tinka <tinka.valentijn@aalto.fi>
2020-01-22 12:56:11 +0100

    Experiment with sampler

--- caladrius/model/data.py
+++ caladrius/model/data.py
@@ -3,6 +3,68 @@ from PIL import Image
 from tqdm import tqdm
 
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import RandomSampler
+import torch
+import torch.utils.data
+import torchvision
+
+
+class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
+    """Samples elements randomly from a given list of indices for imbalanced dataset
+    Arguments:
+        indices (list, optional): a list of indices
+        num_samples (int, optional): number of samples to draw
+        callback_get_label func: a callback-like function which takes two arguments - dataset and index
+    """
+
+    def __init__(
+        self, dataset, indices=None, num_samples=None, callback_get_label=None
+    ):
+
+        # if indices is not provided,
+        # all elements in the dataset will be considered
+        self.indices = list(range(len(dataset))) if indices is None else indices
+
+        # define custom callback
+        self.callback_get_label = callback_get_label
+
+        # if num_samples is not provided,
+        # draw `len(indices)` samples in each iteration
+        self.num_samples = len(self.indices) if num_samples is None else num_samples
+
+        # distribution of classes in the dataset
+        label_to_count = {}
+        for idx in self.indices:
+            label = self._get_label(dataset, idx)
+            if label in label_to_count:
+                label_to_count[label] += 1
+            else:
+                label_to_count[label] = 1
+
+        # weight for each sample
+        weights = [
+            1.0 / label_to_count[self._get_label(dataset, idx)] for idx in self.indices
+        ]
+        self.weights = torch.DoubleTensor(weights)
+
+    def _get_label(self, dataset, idx):
+        if isinstance(dataset, torchvision.datasets.MNIST):
+            return dataset.train_labels[idx].item()
+        elif isinstance(dataset, torchvision.datasets.ImageFolder):
+            return dataset.imgs[idx][1]
+        elif self.callback_get_label:
+            return self.callback_get_label(dataset, idx)
+        else:
+            raise NotImplementedError
+
+    def __iter__(self):
+        return (
+            self.indices[i]
+            for i in torch.multinomial(self.weights, self.num_samples, replacement=True)
+        )
+
+    def __len__(self):
+        return self.num_samples
 
 
 class CaladriusDataset(Dataset):
@@ -68,9 +130,11 @@ class Datasets(object):
         data_loader = DataLoader(
             dataset,
             batch_size=self.batch_size,
-            shuffle=(set_name == "train"),
+            # shuffle=(set_name == "train"),
             num_workers=self.number_of_workers,
             drop_last=True,
+            sampler=RandomSampler(dataset) if (set_name == "train") else None,
+            # sampler=ImbalancedDatasetSampler(dataset),
         )
 
         return dataset, data_loader
